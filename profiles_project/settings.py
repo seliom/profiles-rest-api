fd19df2957bb8838886bdc41b5c1480e208ff365
Seliom Gobeze <gobezes22@wlu.edu>
2020-12-21 05:11:27 -0600

    Configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'CHANGEME'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1))
 
 ALLOWED_HOSTS = [
     '0.0.0.0'
@@ -125,3 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+STATIC_ROOT = 'static/'
